9e25aae9d79759c3d0e6a0a3f193daefeb1dee21
yuvrxj-afk <yuvrajsisodiya7302@gmail.com>
2025-10-05 02:14:15 +0530

    feat: seller service

---
+++ seller-service/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const { paymentMiddleware } = require("x402-express");
+
+const app = express();
+
+const routes = {
+  "GET /ad-data": {
+    price: "$0.001",
+    network: "polygon-amoy",
+    config: {
+      description: "Fetch ad placement data",
+    },
+  },
+};
+
+console.log(process.env.PAYMENT_ADDRESS);
+app.use(
+  paymentMiddleware(process.env.PAYMENT_ADDRESS, routes, {
+    url: process.env.FACILITATOR_URL,
+  })
+);
+
+app.get("/ad-data", (req, res) => {
+  res.json({
+    adContent: "Buy 1 Get 1 Free",
+    publisher: "uv",
+    timestamp: new Date(),
+  });
+});
+
+app.listen(4021, () => {
+  console.log("Seller service running at http://localhost:4021");
+});
